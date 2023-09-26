228f006a6fd101128ba4ced063722a747eabc79a
pradeep <724pradeep@gmail.com>
2023-09-26 06:56:21 +0530

    Reducer is added

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const BackDrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const OverLay = (props) => {
@@ -19,7 +19,10 @@ const protalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<BackDrop />, protalElement)}
+      {ReactDOM.createPortal(
+        <BackDrop onClose={props.onClose} />,
+        protalElement
+      )}
       {ReactDOM.createPortal(
         <OverLay>{props.children}</OverLay>,
         protalElement
---
+++ src/store/CartProvider.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = { items: [], totalAmount: 0 };
+
+const cartReducer = (state, action) => {
+  if (action.type === "Add") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price + action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "Add", item: item });
+  };
+  const removerItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removerItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
